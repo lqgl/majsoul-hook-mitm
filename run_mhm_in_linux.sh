881fa36b5c76e40403b83002c9f28fa7eacf459a
Nico w10 in vmware <@ .com>
2024-03-02 16:26:27 +0800

    feat: 增加了一个linus下的启动sh
    1.增加了一个linus下的启动sh

--- run_mhm_in_linux.sh
+++ run_mhm_in_linux.sh
@@ -1,7 +1,7 @@
 . venv/bin/activate
 #代理到clash
 export all_proxy=socks5://127.0.0.1:7890/
-export all_proxy=socks5://127.0.0.1:7890/
+export ALL_PROXY=socks5://127.0.0.1:7890/
 python -m mhm
 read -s -n1 -p "按任意键继续 ... "
 echo ""
